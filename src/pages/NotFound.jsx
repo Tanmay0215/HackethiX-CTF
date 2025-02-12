32edd05ad5116d261b6e6956d42a7d8546dc118c
Tanmay0215 <tanmaygupta.0215@gmail.com>
2025-02-12 18:53:12 +0530

    matrix effect and notfound page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,23 @@
+import { Link } from 'react-router-dom'
+import MatrixBackground from '../components/MatrixBackground'
+import ReactTypingEffect from 'react-typing-effect'
+
 const NotFound = () => {
-    return (
-        <div className="flex flex-col w-[100%] h-[100vh] text-center justify-center bg-black text-white">
-            <div className="text-4xl p-5">Error 404: Page Not Found</div>
-            <div className="text-2xl">Congrats!! You found another flag<p>CTF 3rr0r_p4g3</p></div>
-            
-        </div>
-    )
+  return (
+    <div className="flex h-screen text-center items-center justify-center text-white">
+      <MatrixBackground />
+      <div className="absolute z-10 w-full mx-auto flex flex-col">
+        <ReactTypingEffect
+          text={['Error 404: Page Not Found', 'CTF{3rr0r_p4g3}']}
+          speed={100}
+          eraseSpeed={50}
+          eraseDelay={2000}
+          typingDelay={500}
+          className="text-5xl mb-5"
+        />
+        <Link to="/">Back to Home</Link>
+      </div>
+    </div>
+  )
 }
-export default NotFound;
+export default NotFound
