765f9b67c3789efe1565959ffdf1d35c4825249c
Tanmay0215 <tanmaygupta.0215@gmail.com>
2025-02-27 16:34:03 +0530

    add Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,133 +1,58 @@
-import ReactTypingEffect from "react-typing-effect";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import Content from "../components/Content";
-import "./App.css";
-import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import ReactTypingEffect from 'react-typing-effect'
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import Content from '../components/Content'
+import { useEffect } from 'react'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
 
 function App() {
-  const navigate = useNavigate();
-
+  
   useEffect(() => {
-    document.cookie =
-      "debug_info=Q1RGe2Nvb2tpZV9tb25zdGVyfQ==; path=/; Secure;";
-  },);
+    document.cookie = 'debug_info=Q1RGe2Nvb2tpZV9tb25zdGVyfQ==; path=/; Secure;'
+    console.log('Looking for flags? CTF{c0ns0l3_l0g}')
+  })
 
   const notifySuccess = () =>
-    toast.success("🎉 Flag Unlocked!! CTF{cl1ck3r_g4m3_pr0}");
+    toast.success('🎉 Flag Unlocked!! CTF{cl1ck3r_g4m3_pr0}')
 
-  function logFlag() {
-    console.log("Looking for flags? CTF{c0ns0l3_l0gg3r}");
-  }
-
-  setInterval(logFlag, 60000);
-
-  // async function callbackend() {
-  //   const res = await fetch("http://localhost:8000/api/status", {
-  //     method: "GET",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //       "X-CTF-Flag": "CTF{h34d3r_sp0tt3r}",
-  //     },
-  //   });
-  //   const data = await res.json();
-  //   console.log(data);
-  // }
-
-  // callbackend();
 
   function secretButton() {
-    let clicks = 0;
-    clicks++;
+    let clicks = 0
+    clicks++
     if (clicks === 1) {
-      notifySuccess();
+      notifySuccess()
       // console.log("Flag Unlocked!! CTF{cl1ck3r_g4m3_pr0}");
     }
   }
   return (
-    <div className="h-screen w-screen">
-      <ToastContainer
-        position="top-center"
-        autoClose={5000}
-        hideProgressBar={false}
-        closeOnClick={true}
-        pauseOnHover={true}
-        draggable={true}
-        pauseOnFocusLoss={false}
+    <div className="h-screen flex flex-col relative bg-gray-900 text-white">
+      <Navbar />
+      <h1 className="text-2xl font-bold mb-4 p-5 text-green-500">
+        <ReactTypingEffect
+          text={['Are you ready!!', 'Find 10 Flags to win the game!!']}
+          speed={75}
+          eraseSpeed={50}
+          cursor={'_'}
+        />
+      </h1>
+
+      <img
+        src="ctf.jpeg"
+        alt="CTF"
+        className="w-full max-w-2xl mx-auto mb-4 rounded shadow-lg"
       />
 
-      <nav className="navbar">
-        <h2 className="text-3xl text-gray-400 font-bold p-3 px-16">Hackethix CTF Challenge</h2>
-        <div className="flex gap-3 p-3">
-        <button
-          className="login"
-          onClick={() => {
-            navigate("/submit");
-          }}
-        >
-          Submit
-        </button>
+      <button
+        onClick={secretButton}
+        className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Reveal a Flag
+      </button>
 
-        <button
-          className="login"
-          onClick={() => {
-            navigate("/leaderboard");
-          }}
-        >
-          Leaderboard
-        </button>
-
-        <button
-          className="login"
-          onClick={() => {
-            navigate("/login");
-          }}
-        >
-          Login
-        </button>
-        </div>
-      </nav>
-
-      <main>
-        <section>
-          <h1 style={{ fontSize: "56px", padding: "10px" }}>
-            <ReactTypingEffect
-              text={["Are you ready!!", "Find 10 Flags to win the game!!"]}
-              speed={75}
-              eraseSpeed={50}
-              cursor={"_"}
-              className="text-cyan-400 font-bold font-mono italic"
-            />
-          </h1>
-
-          <img src="ctf.jpeg" alt="" style={{ width: "100%" }} />
-
-          <button onClick={secretButton}>Reveal a Flag</button>
-        </section>
-        <Content />
-      </main>
-      <footer>
-        <div>
-          <h4>Follow Us</h4>
-          <a href="./Flag" target="_blank">
-            <p>LinkedIn</p>
-            <p>Youtube</p>
-          </a>
-        </div>
-
-        <div>
-          <h4>Join Our 3-Day Bootcamp</h4>
-          <a href="https://yourwebsite.com/bootcamp">
-            Click here to learn more and register!
-          </a>
-        </div>
-
-        <div>
-          <p>&copy; 2024 Ticketing Platform. All rights reserved.</p>
-        </div>
-      </footer>
+      <Content />
+      <Footer />
     </div>
-  );
+  )
 }
-export default App;
+export default App
